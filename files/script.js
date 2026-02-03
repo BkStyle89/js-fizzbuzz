bd2162da31dd67c5b3bfcbd15404af5350e3826f
BkStyle89 <kajzer@hotmail.it>
2026-02-03 15:17:54 +0100

    primo ciclo con commento

--- files/script.js
+++ files/script.js
@@ -5,3 +5,9 @@ console.log('i can see the file');
 // numeri stamptati con multipli di 5 con buzz
 // numeri stampati sia con 3 che 5 fizzbuzz
 
+
+/* ciclo con commento da 1 a 100 */
+for (let i=1; i<=100; i++){
+    console.log(i);
+    
+}

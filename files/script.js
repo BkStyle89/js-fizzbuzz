01216d97177b12945b64ee4c02ec46b8bce227f4
BkStyle89 <kajzer@hotmail.it>
2026-02-03 15:24:08 +0100

    ciclo Buzz * 5 con commento

--- files/script.js
+++ files/script.js
@@ -9,8 +9,13 @@ console.log('i can see the file');
 /* ciclo con commento da 1 a 100 */
 for (let i=1; i<=100; i++){
     console.log(i);
-/* ciclo * 3 con commento */
+/* ciclo * 3 Fizz con commento */
     if (i%3 == 0) {
         console.log(i,'Fizz');
+    }
+/* ciclo * 5 Buzz con commento */
+    else if (i%5 == 0) {
+        console.log(i, 'buzz');
+            
     }
 }

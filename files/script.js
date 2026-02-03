c9901c112b9f8900185e5947564421c06c151e2b
BkStyle89 <kajzer@hotmail.it>
2026-02-03 15:41:47 +0100

    ciclo *3*5 BuzzFizz e correzione

--- files/script.js
+++ files/script.js
@@ -9,13 +9,17 @@ console.log('i can see the file');
 /* ciclo con commento da 1 a 100 */
 for (let i=1; i<=100; i++){
     console.log(i);
-/* ciclo * 3 Fizz con commento */
-    if (i%3 == 0) {
-        console.log(i,'Fizz');
+/* ciclo con il *3 *5 BuzzFizz */
+    if (i%3 == 0 && i%5 == 0){
+        console.log(i, 'BuzzFizz');  
     }
+ /* ciclo * 3 Fizz con commento */   
+    else if (i%3 == 0) {
+        console.log(i,'Fizz');
 /* ciclo * 5 Buzz con commento */
+    }
     else if (i%5 == 0) {
-        console.log(i, 'buzz');
-            
+        console.log(i,'Buzz');
+        
     }
 }

9b9af4e20dab15b9617d5f6c3f5fdec60939998a
BkStyle89 <kajzer@hotmail.it>
2026-02-03 15:14:01 +0100

    commenti in italiano

---
+++ files/script.js
@@ -0,0 +1,7 @@
+console.log('i can see the file');
+
+//numeri stampati da 1 a 100
+// i numeri multipli di 3 stmpati con fizz
+// numeri stamptati con multipli di 5 con buzz
+// numeri stampati sia con 3 che 5 fizzbuzz
+
